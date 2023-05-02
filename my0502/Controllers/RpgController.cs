a6142970cfa3720067dbdb7572365765f554427c
kurosetsuki <bettycheng0801@gmail.com>
2023-05-02 16:44:25 +0800

    get post

--- my0502/Controllers/RpgController.cs
+++ my0502/Controllers/RpgController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using my0502.Services;
 
 namespace my0502.Controllers
 {
@@ -11,13 +12,28 @@ namespace my0502.Controllers
     [ApiController]
     public class RpgController : ControllerBase
     {
+        private Rpgservice _service;
+
+        public RpgController()
+        {
+            _service = new Rpgservice();
+        }
+        
         [HttpGet]
-        public Rpg Get()
+        public Rpg Get(int id)
         {
             Rpg result = null;
 
-            result = new Rpg() { id = 0, name = "boss", lv = 999 };
+            result =_service.Get(id);
 
+            return result;
+        }
+
+
+        public Rpg Post(Rpg rpg)
+        {
+            Rpg result = null;
+            result = _service.Create(rpg);
             return result;
         }
     }
---
+++ my0502/Services/Rpgservice.cs
@@ -0,0 +1,45 @@
+﻿namespace my0502.Services;
+
+public class Rpgservice
+{
+
+    private static IList<Rpg> database;
+
+    static Rpgservice()
+    {
+        database = new List<Rpg>();
+        database.Add(new Rpg(){ id = 0, name = "boss", lv = 999 });
+        database.Add(new Rpg(){ id = 1, name = "史萊姆", lv = 1 });
+        database.Add(new Rpg(){ id = 2, name = "家門鳥", lv = 2 });
+        database.Add(new Rpg(){ id = 3, name = "蟲", lv = 3 });
+    }
+    
+    //Get/Read
+    public Rpg Get(int id)
+    {
+        Rpg result = null;
+
+        var db = from d in database
+            where d.id == id
+            select d;
+        if (db.Any())
+        {
+            result = db.First();
+        }
+
+        return result;
+    }
+    
+    
+    //Post/Create
+    public Rpg Create(Rpg rpg)
+    {
+        //Rpg result = null;
+
+        rpg.id = database.Max(x => x.id) + 1; //修正id
+        
+        database.Add(rpg);
+
+        return rpg;
+    }
+}
